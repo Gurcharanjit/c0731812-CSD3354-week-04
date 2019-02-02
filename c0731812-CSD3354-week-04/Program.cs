efede4af9c4dcc60a42fd5d6fb746264cebad4cf
Gurcharanjit singh <montymultani13@gmail.com>
2019-02-01 19:40:53 -0500

    C0731812-CSD3354-WEEK-04

--- c0731812-CSD3354-week-04/Program.cs
+++ c0731812-CSD3354-week-04/Program.cs
@@ -6,10 +6,32 @@ using System.Threading.Tasks;
 
 namespace c0731812_CSD3354_week_04
 {
+
     class Program
     {
         static void Main(string[] args)
         {
+            countryside c = new countryside();
+
         }
     }
+    class village
+    {
+        bool isAstrildeHere;
+        village nextvillage;
+        village prevvillage;
+        String villagename;
+
+
+    }
+
+
+    class countryside
+    {
+        village mapple;
+        village toronto;
+        village ajax;
+    }
 }
+
+
